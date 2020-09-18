6ab8b9d67794578eec0d3c5ec25a24194d879f5a
pcollender <pcollender@berkeley.edu>
2020-09-18 13:00:42 -0700

    ..

--- Chile/ASmass_Water_vs_ASmass_Urine_scatterplots.R
+++ Chile/ASmass_Water_vs_ASmass_Urine_scatterplots.R
@@ -1,7 +1,9 @@
 library(tidyverse)
 library(lubridate)
-ar.dat = readxl::read_xlsx('FOR_ANDRES_012920.xlsx')
-chile<-read.csv("Chile_Arsenic_Data_Redux.csv")
+
+##data loading and variable manipulations
+ar.dat = readxl::read_xlsx('..\\..\\..\\FOR_ANDRES_012920.xlsx')
+chile<-read.csv("..\\..\\..\\Chile_Arsenic_Data_Redux.csv")
 chile = chile %>% mutate(Cancer = !is.na(Other_cancer) | Cancer_type != 'control')
 
 chile = chile %>% group_by(SubjectID) %>% mutate(yrin = year(DateEnr)) %>% 
@@ -10,7 +12,7 @@ chile = chile %>% group_by(SubjectID) %>% mutate(yrin = year(DateEnr)) %>%
 
 chile = chile %>% filter(!duplicated(SubjectID))
 
-#### Create new var (WaterArsenic2) to replace 0 values with 0.1
+#Create new var (WaterArsenic2) to replace 0 values with 0.1
 chile$WaterAS1yr2<-ifelse(chile$WaterAS1yr==0,0.1,chile$WaterAS1yr)
 chile$WaterAS6yr2<-ifelse(chile$WaterAS6yr==0,0.1,chile$WaterAS6yr)
 
@@ -26,13 +28,13 @@ chile$allmuni = c('All municipal water','Not all municipal water')[2-(chile$prop
 
 chile$water_intake = ar.dat$TKHL[match(chile$SubjectID,ar.dat$commonID)]
 
-propdat = readxl::read_xlsx('WATER_INFO_060520.xlsx')
+propdat = readxl::read_xlsx('..\\..\\..\\WATER_INFO_060520.xlsx')
 chile$prop_muni = propdat$percent_muni[match(chile$SubjectID,propdat$commonID)]/100
 
-#get mean water intake
+##get mean water intake
 mean_water_intake = mean(chile$water_intake)
 
-#get conditional mean urine volume
+##get conditional mean urine volume
 ur.dat = read.csv('TFI_vs_urine.csv')
 ur.lm = lm(Urine_24 ~ TFI, ur.dat)
 
@@ -53,7 +55,9 @@ ave.lm = lm(I(mean_urine_volume * UrinaryArsenic) ~ I(mean_water_intake * WaterA
 ind.lm = lm(I(coef(ur.lm)[1] + coef(ur.lm)[2] * water_intake * UrinaryArsenic) ~ 
               I(water_intake * WaterAS1yr2), data = chile)
 
-#using overall mean water intake
+#scatterplot using overall mean water intake
+png(file = 'Chile_aggregate_AS_mass_Water_vs_Urine.png',width = 360, height = 360)
+
 ggplot(chile, aes(x = mean_water_intake * WaterAS1yr2, y = mean_urine_volume * UrinaryArsenic)) + 
   geom_point() + geom_smooth(method = 'lm') + labs(x = 'Mass Arsenic Ingested in Water (µg / day)',
                                                    y = 'Mass Arsenic Excreted in Urine (µg / day)',
@@ -62,8 +66,10 @@ ggplot(chile, aes(x = mean_water_intake * WaterAS1yr2, y = mean_urine_volume * U
   theme_bw() + 
   theme(plot.title = element_text(hjust = 0.5))
 
+dev.off()
+#scatterplot using reported water intake
+png(file = 'Chile_individual_AS_mass_Water_vs_Urine.png',width = 360, height = 360)
 
-#using reported water intake
 ggplot(chile, aes(x = water_intake * WaterAS1yr2, 
                   y = coef(ur.lm)[1] + coef(ur.lm)[2] * water_intake * UrinaryArsenic)) + 
   geom_point() + geom_smooth(method = 'lm') + labs(x = 'Mass Arsenic Ingested in Water (µg / day)',
@@ -73,3 +79,4 @@ ggplot(chile, aes(x = water_intake * WaterAS1yr2,
   theme_bw() + 
   theme(plot.title = element_text(hjust = 0.5))
 
+dev.off()
